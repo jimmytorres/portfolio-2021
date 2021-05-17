cb91dbcca13b16f5422912c937e0684149cc5f0e
Jimmy <jtorres244@west-mec.org>
2021-05-17 16:27:52 -0700

    Created the contact section

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
 })
 export class AppComponent {
   title = 'my-app';
+
+  outputPath:string = 'www.linkedin.com/in/jimmy-torres-3224621b5';
+  outputPath2:string = 'www.github.com/jimmytorres'
+
+  onLinkedin(){
+    window.location.href = "www.linkedin.com/in/jimmy-torres-3224621b5"
+  }
+
 }
